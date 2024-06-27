b4dcc32167660bd70588eec5d1c6640b81e97fa6
hardarmyyy <adeyemoadams005@gmail.com>
2024-06-27 08:33:38 +0100

    FEAT: add a new url to allowed origins

--- SERVER/Utilities/allowedOrigins.js
+++ SERVER/Utilities/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'https://www.google.com', 
     'http://localhost:4080/', 
     'http://localhost:5173',
-    'https://pick-n-pay-app.vercel.app'
+    'https://pick-n-pay-app.vercel.app',
+    'https://5n2ps3ww-5173.euw.devtunnels.ms'
 ] // last 2 should be removed after development
 
 module.exports = {
