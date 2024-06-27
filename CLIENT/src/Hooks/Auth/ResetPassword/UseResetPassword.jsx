c04e0e8dec1b392896f1fa75711ee91444b7e666
hardarmyyy <adeyemoadams005@gmail.com>
2024-06-27 13:24:11 +0100

    FIX: modify reset password and use reset password hook for form submission

--- CLIENT/src/Hooks/Auth/ResetPassword/UseResetPassword.jsx
+++ CLIENT/src/Hooks/Auth/ResetPassword/UseResetPassword.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { RESETPASSWORD, VERIFYRESETTOKEN } from '../../../Services/authApi'
 import UseValidateResetPasswordForm from './UseValidateResetPasswordForm'
 import {toast} from 'react-toastify'
@@ -7,112 +7,111 @@ import {toast} from 'react-toastify'
 
 const UseResetPassword = (email, token) => {
 
-const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const isValid = useSelector((state) => state?.auth.isValid)
+    const status = useSelector((state) => state.auth.status)
+    const verifyResetStatus = useSelector((state) => state?.auth?.status)
 
-const [user, setUser] = useState({
-    password: '',
-    confirmPassword: '',
-}) 
+    const [user, setUser] = useState({
+        password: '',
+        confirmPassword: '',
+    }) 
 
-const [error, setError] = useState({})
-const [invalid, setInvalid] = useState({})
+    const [error, setError] = useState({})
 
-const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-const [openModal, setOpenModal] = useState(false) 
+    const [openModal, setOpenModal] = useState(false) 
 
-// define a state to handle show password
-const [showPassword, setShowPassword] = useState(false)
-const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+    // define a state to handle show password
+    const [showPassword, setShowPassword] = useState(false)
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
 
-const handleChange = (e) => {
-    const {name, value} = e.target;
-    setUser((user) => {return {...user, [name]: value.replace(/\s/g, "")}})
-    // validating form state;
-    if (name === 'password') {
-        setError((error) => {return {...error, password: value ? '': 'Kindly enter password' }})
-        setInvalid((invalid) => {return {...invalid, password: value ? false : true}})
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+
+        setUser((user) => {return {...user, [name]: value.replace(/\s/g, "")}});
+        // validating form state;
+        if (name === 'password') {
+            setError((error) => {return {...error, password: value ? value.length > 8 ? '' : 'Password must be at least 8 characters' : 'Kindly enter password' }})
+        }
+        if (name === 'confirmPassword') {
+            const password = user.password
+            setError((error) => {return {...error, confirmPassword: value ? 
+                                                                            password === value ? '' : 'Password does not match' 
+                                                                                : 'Kindly confirm password' }})
+        }
     }
-    if (name === 'confirmPassword') {
-        setError((error) => {return {...error, confirmPassword: value ? '': 'Kindly confirm password' }})
-        setInvalid((invalid) => {return {...invalid, confirmPassword: value ? false : true}})
+
+    // define a function to open modal 
+    const handleOpenModal = () => { 
+        setOpenModal(!openModal);
+    } 
+
+    // define a function to handle show password;
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword)
     }
-}
+    const handleShowConfirmPassword = () => {
+        setShowConfirmPassword(!showConfirmPassword)
+    }
 
-// define a function to open modal 
-const handleOpenModal = () => { 
-    setOpenModal(!openModal);
-} 
+    const {errors} = UseValidateResetPasswordForm(user) //import UseValidateResetPasswordForm hook to catch errors on the form object 
 
-// define a function to handle show password;
-const handleShowPassword = () => {
-    setShowPassword(!showPassword)
-}
-const handleShowConfirmPassword = () => {
-    setShowConfirmPassword(!showConfirmPassword)
-}
+    const canSubmit = (value) => {
+        const isSave = [
+            value.password && 
+            value.confirmPassword  &&
+            value.password.length >= 8 &&
+            value.password == value.confirmPassword
+        ].every(Boolean) 
+        return isSave
+    }
 
-const {errors, invalids} = UseValidateResetPasswordForm(user) //import UseValidateResetPasswordForm hook to catch errors on the form object 
-
-const passwordRegexUpperCase = /^(?=.*[A-Z])[a-zA-Z0-9.!@#$%^&*]{8,}$/;
-const passwordRegexNumber = /^(?=.*[0-9])[a-zA-Z0-9.!@#$%^&*]{8,}$/;
-const passwordRegexSymbol = /^(?=.*[.!@#$%^&*])[a-zA-Z0-9.!@#$%^&*]{8,}$/;
-
-const canSubmit = (value) => {
-    const isSave = [
-        value.password, 
-        value.confirmPassword,
-        passwordRegexUpperCase.test(value.password),
-        passwordRegexNumber.test(value.password),
-        passwordRegexSymbol.test(value.password)
-    ].every(Boolean) 
-    return isSave
-}
-
-const submitEmail = canSubmit(user)
-
-const handleResetPassword = async () => {
-
-    setError(errors)
-
-    if (isSubmitting) return; 
+    const submitEmail = canSubmit(user)
     
-    if(submitEmail && !isSubmitting) {
-        setIsSubmitting(true); // Disable the button
+    const handleResetPassword = async () => {
+
+        setError(errors)
+
+        if (isSubmitting) return; 
         
-        await  dispatch(RESETPASSWORD({user, token}))
-        .then((response) => {
-            if (response.payload.message) {
-                setTimeout(() => {
-                    handleOpenModal()
-                }, 2500)
-                setUser({
-                    password: '',
-                    confirmPassword: '',
-                })
-            }
-        })
-        .catch((err) => {
-            toast.error('Something went wrong', {
-                toastStyle: { background: 'red', color: 'white' }
+        if(submitEmail && !isSubmitting) {
+            setIsSubmitting(true); // Disable the button
+            
+            await  dispatch(RESETPASSWORD({user, token}))
+            .then((response) => {
+                if (response.payload.message) {
+                    setTimeout(() => {
+                        handleOpenModal()
+                    }, 2500)
+                    setUser({
+                        password: '',
+                        confirmPassword: '',
+                    })
+                }
             })
-        })
-        .finally(() => {
-            setIsSubmitting(false); 
-        })
+            .catch((err) => {
+                toast.error('Something went wrong', {
+                    toastStyle: { background: 'red', color: 'white' }
+                })
+            })
+            .finally(() => {
+                setIsSubmitting(false); 
+            })
+        }
     }
-}
 
-const handleVerifyResetToken = () => {
-    dispatch(VERIFYRESETTOKEN({token, email})) 
-}
+    const handleVerifyResetToken = () => {
+        dispatch(VERIFYRESETTOKEN({token, email})) 
+    }
 
-useEffect(() => {
-    canSubmit(user)
-}, [user])
+    useEffect(() => {
+        canSubmit(user)
+    }, [user])
 
-return {user, error, invalid, handleChange, handleVerifyResetToken, handleResetPassword, openModal, handleOpenModal, showPassword, handleShowPassword, showConfirmPassword, handleShowConfirmPassword}
+    return {isValid, status, verifyResetStatus, user, error, handleChange, handleVerifyResetToken, handleResetPassword, openModal, handleOpenModal, showPassword, handleShowPassword, showConfirmPassword, handleShowConfirmPassword}
 }
 
 export default UseResetPassword
--- CLIENT/src/Hooks/Auth/ResetPassword/UseValidateResetPasswordForm.jsx
+++ CLIENT/src/Hooks/Auth/ResetPassword/UseValidateResetPasswordForm.jsx
@@ -3,48 +3,27 @@ import { useState, useEffect } from "react";
 const UseValidateResetPasswordForm = (value) => {
 
 const [errors, setError] = useState({})
-const [invalids, setInvalid] = useState({})
-
-// Regex for form values
-// const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[.!@#$%^&*])[a-zA-Z0-9.!@#$%^&*].{8,}$/;
-const passwordRegexUpperCase = /^(?=.*[A-Z])[a-zA-Z0-9.!@#$%^&*]{8,}$/;
-const passwordRegexNumber = /^(?=.*[0-9])[a-zA-Z0-9.!@#$%^&*]{8,}$/;
-const passwordRegexSymbol = /^(?=.*[.!@#$%^&*])[a-zA-Z0-9.!@#$%^&*]{8,}$/;
 
 
 const validate = (value) => {
     const newErrors = {};
-    const newInvalid = {};
 
     for (const field in value) {
         if (field === 'password' && value[field].trim() === '') {
             newErrors[field] = 'Password field is required';
-            newInvalid[field] = true;
         }
         else if (field === 'password' && value[field].trim() !== '' && value[field].length < 8 ) {
             newErrors[field] = 'Password must be at least 8 characters';
-            newInvalid[field] = true;
-        }
-        else if ((field === 'password' && value[field].trim() !== '') && !(passwordRegexUpperCase.test(value[field]))) {
-            newErrors[field] = 'Password must contain at least one uppercased letter!';
-            newInvalid[field] = true;
-        }
-        else if ((field === 'password' && value[field].trim() !== '') && !(passwordRegexNumber.test(value[field]))) {
-            newErrors[field] = 'Password must contain at least one number!';
-            newInvalid[field] = true;
-        }
-        else if ((field === 'password' && value[field].trim() !== '') && !(passwordRegexSymbol.test(value[field]))) {
-            newErrors[field] = 'Password must contain at least one symbol!';
-            newInvalid[field] = true;
         }
         else if (field === 'confirmPassword' && value[field].trim() === '') {
             newErrors[field] = 'Confirm password field is required';
-            newInvalid[field] = true;
+        }
+        else if (field === 'confirmPassword' && value[field].trim() !== '' && value[field] !== value.password ) {
+            newErrors[field] = 'Password does not match';
         }
     }
 
     setError(newErrors)
-    setInvalid(newInvalid)
 }
 
 useEffect(() => {
@@ -52,7 +31,7 @@ useEffect(() => {
 }, [value]) 
 
 
-return {errors, invalids}
+return {errors}
 }
 
 export default UseValidateResetPasswordForm
