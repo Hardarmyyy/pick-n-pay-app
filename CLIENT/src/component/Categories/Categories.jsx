91dcfa6aa995289691233d25391a3f190235858f
hardarmyyy <adeyemoadams005@gmail.com>
2024-06-30 17:10:59 +0100

    FIX: update categpries

--- CLIENT/src/component/Categories/Categories.jsx
+++ CLIENT/src/component/Categories/Categories.jsx
@@ -1,28 +1,10 @@
 import { Link } from "react-router-dom"
-import { CATEGORYPRODUCTS } from '../../Services/categoryApi'
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from "react-router-dom"
-
+import { useSelector } from 'react-redux'
 
 
 const Categories = ({eventHandler}) => {
 
-const dispatch = useDispatch()
-const navigate = useNavigate()
 const allCategories = useSelector((state) => state?.category.allCategories)
-
-const fetchCategoryProducts = async (category) => {
-    await dispatch(CATEGORYPRODUCTS(category))
-    .then((response) => {
-        if (response.payload.success) {
-            navigate(`/category/${category}`)
-        }
-    })
-    .catch((error) => {
-        console.error(error)
-    })
-}
-
 
 return (
 
@@ -30,7 +12,7 @@ return (
     
 
         <ul className="tablet:ms-4 mini:ms-6 laptop:ms-10 super:ms-10">
-            {allCategories.map((category) =>
+            {allCategories?.map((category) =>
                 <li key={category?.categoryID} onClick={eventHandler} className="transition duration-75 text-sm my-1 py-1">
                     <Link to={`/category/${category.category}`}> 
                         {category.category}
