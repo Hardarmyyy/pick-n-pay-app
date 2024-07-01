c80a7594edf0d4ccc4fa2155d973fa5930d52f1d
hardarmyyy <adeyemoadams005@gmail.com>
2024-07-01 09:11:01 +0100

    FIX: update persist login

--- CLIENT/src/Utils/PersistLogin.jsx
+++ CLIENT/src/Utils/PersistLogin.jsx
@@ -1,14 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { REFRESH } from "../Services/authApi";
+import Navigation from "../Layouts/Navigation/Navigation";
+import Footer from "../Layouts/Footer/Footer";
+import Modal from "../component/Modal";
 import Spinner from "../component/Spinner";
 
 const PersistLogin = () => {
 
+    const {category} = useParams()
     const [isLoading, setIsLoading] = useState(true);
     const dispatch = useDispatch();
     const accessToken = useSelector((state) => state?.auth?.accessToken)
+    const logoutStatus = useSelector((state) => state?.auth?.status)
+    const refreshStatus = useSelector((state) => state?.auth?.status)
 
     useEffect(() => {
 
@@ -30,10 +36,19 @@ const PersistLogin = () => {
 
     return (
         <>
-            {isLoading 
-                ? <div className="loader"> <Spinner></Spinner> </div>
+            {logoutStatus === 'Loading...' && <Modal> <Spinner></Spinner> </Modal>}
+
+            <Navigation category={category}></Navigation>
+
+            {isLoading && refreshStatus !== 'Loading.......'
+                ? 
+                    <section className='w-full h-1/2 tablet:h-1/2 mini:h-3/5 laptop:h-3/4 super:h-3/4 flex justify-center items-center py-6 sm:p-2 md:p-2 tablet:px-4 mini:px-6 laptop:px-6 super:px-60'>
+                        <Spinner></Spinner> 
+                    </section>
                 : <Outlet></Outlet>
             }
+
+            <Footer></Footer>
         </>
     )
 
