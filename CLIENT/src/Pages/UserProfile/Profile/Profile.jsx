99dd4974f4c77b59a0436cc71c0ad28b19e5b5d5
hardarmyyy <adeyemoadams005@gmail.com>
2024-07-14 17:23:22 +0100

    FIX: update and add resposiveness to profile component

--- CLIENT/src/Pages/UserProfile/Profile/Profile.jsx
+++ CLIENT/src/Pages/UserProfile/Profile/Profile.jsx
@@ -4,51 +4,49 @@ import { useSelector } from 'react-redux';
 
 const Profile = () => {
 
-const currentUser = useSelector((state) => state.auth?.user)
+const currentUser = useSelector((state) => state?.user?.user)
 const buyer = currentUser?.userRole[0]
 const seller = currentUser?.userRole[0]
 
-
   return (
     <>
-            <section className='w-1/2 p-10 font-Montserrat text-my-primary border rounded-md shadow-sm bg-white mx-auto translate-y-0'>
+            <section className='w-full tablet:w-3/4 mini:w-3/5 laptop:w-1/2 super:w-3/5 mx-auto'>
 
-                <p className='text-center mb-2 font-bold font-Jost md:text-xl lg:text-2xl'> User Information </p>
+                <p className='font-Jost text-lg tablet:text-xl mini:text-2xl laptop:text-3xl super:text-3xl mb-2 text-blue-950 font-bold text-center'> User Information </p>
 
-                <div className='lg:w-96 xl:w-full md:text-sm lg:text-lg mt-2'>
+                <form className='w-full h-auto text-sm font-Montserrat text-my-primary bg-[#fcfcfc] rounded-md py-3 sm:px-2 md:px-4 tablet:px-4 mini:px-5 laptop:px-4 flex flex-col justify-center items-center'>
 
-                    <form className='w-full'>
-                        <div className='flex justify-center items-center'>
-                            <label className='font-bold'> Usertype: </label>
-                            <input 
-                                type="text" 
-                                value={buyer ? buyer : seller} 
-                                disabled
-                                className='w-full ml-2 mt-2 px-2 border rounded-md shadow-sm bg-white placeholder:italic placeholder:text-slate-400'
-                            />
-                        </div>
-                        <div className='flex justify-between items-center'>
-                            <label className='font-bold'> Username: </label>
-                            <input 
-                                type="text" 
-                                value={currentUser?.userName} 
-                                disabled
-                                className='w-full ml-2 mt-2 px-2 border rounded-md shadow-sm bg-white placeholder:italic placeholder:text-slate-400'
-                            />
-                        </div>
-                        <div className='flex justify-between items-center'>
-                            <label className='font-bold'> Email: </label>
-                            <input 
-                                type="text" 
-                                value={currentUser?.email} 
-                                disabled
-                                className='w-full ml-2 mt-2 px-2 border rounded-md shadow-sm bg-white placeholder:italic placeholder:text-slate-400'
-                            />
-                        </div>
-                        
-                    </form>
+                    <div className='w-full flex justify-between items-center mt-2'>
+                        <label className='font-bold text-sm flex-shrink-0'> Usertype: </label>
+                        <input 
+                            type="text" 
+                            value={buyer ? buyer : seller} 
+                            disabled
+                            className='w-full border rounded-md shadow-sm text-sm ms-2 bg-white placeholder:italic placeholder:text-slate-400'
+                        />
+                    </div>
 
-                </div>
+                    <div className='w-full flex justify-between items-center mt-2'>
+                        <label className='font-bold text-sm flex-shrink-0'> Username: </label>
+                        <input 
+                            type="text" 
+                            value={currentUser?.username} 
+                            disabled
+                            className='w-full border rounded-md shadow-sm text-sm ms-2 bg-white placeholder:italic placeholder:text-slate-400'
+                        />
+                    </div>
+
+                    <div className='w-full flex justify-between items-center mt-2'>
+                        <label className='font-bold text-sm flex-shrink-0'> Email: </label>
+                        <input 
+                            type="text" 
+                            value={currentUser?.email} 
+                            disabled
+                            className='w-full border rounded-md shadow-sm text-sm ms-2 bg-white placeholder:italic placeholder:text-slate-400'
+                        />
+                    </div>
+                    
+                </form>
 
             </section>
     </>
