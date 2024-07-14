d66deee2af71233f84574e759d6cc2573a149b9a
hardarmyyy <adeyemoadams005@gmail.com>
2024-07-14 17:25:47 +0100

    FIX: modify delete profile modal

--- CLIENT/src/Layouts/DeleteProfileModal/DeleteProfileModal.jsx
+++ CLIENT/src/Layouts/DeleteProfileModal/DeleteProfileModal.jsx
@@ -1,33 +1,49 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import UseDeleteUser from '../../Hooks/Profile/UseDeleteUser'
-import Button from '../../component/Button'
+
 
 
 const DeleteProfileModal = ({handleCloseModal}) => {
-
-const status = useSelector((state) => state?.auth?.status)
 
 const {handleDeleteUser} = UseDeleteUser()
 
 return (
 
     <>
-        <div className="bg-my-bg-primary w-full h-full fixed top-0 left-0 z-50"> 
 
-            <div className='w-96 h-96 flex flex-col justify-center items-center font-Montserrat text-my-primary mx-auto rounded-md p-4 bg-white lg:translate-y-28 md:translate-y-20 relative'>
+        <div className="bg-my-bg-primary overflow-y-auto overflow-x-hidden w-full min-h-screen fixed top-0 left-0 z-40"> 
 
-                <p className='text-2xl font-bold my-2 text-center'> Are you sure you want to delete your account  </p>
+            <div className="relative p-4 w-full max-w-md max-h-full popupmodal">
 
-                <div>
-                    <Button  padding='5px 30px' margin='10px auto' backgroundColor='crimson' eventHandler={() =>handleDeleteUser()}> {status === 'Loading.......' ? <span> Deleting</span> : <span> Delete </span>} </Button>
+                <div className="relative bg-white rounded-lg shadow">
+
+                    <button type="button" className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" onClick={handleCloseModal} >
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span className="sr-only"> Close modal </span>
+                    </button>
+
+                    <div className="p-4 md:p-5 text-center">
+                        <svg className="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                        </svg>
+                        <h3 className="mb-5 text-lg font-normal text-gray-500"> Are you sure you want to delete your profile? </h3>
+
+                        <div className='flex justify-center items-center'>
+                            <button type="button" onClick={() =>handleDeleteUser()} className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-4 py-2.5 text-center">
+                                Yes, I'm sure
+                            </button>
+                            <button type="button" className="py-2.5 px-4 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100" onClick={handleCloseModal}> No, cancel </button>
+                        </div>
+                    </div>
+
                 </div>
-
-                <button className='absolute top-4 right-5 text-4xl text-crimson' onClick={handleCloseModal}> X </button>
 
             </div>
 
         </div>
+
     </>
 
 )
