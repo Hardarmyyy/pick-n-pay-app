641e780f53934d8d6b24eae3e457a03db2c465a6
hardarmyyy <adeyemoadams005@gmail.com>
2024-07-20 11:40:03 +0100

    FIX: update delete user hook

--- CLIENT/src/Hooks/Profile/UseDeleteUser.jsx
+++ CLIENT/src/Hooks/Profile/UseDeleteUser.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { DELETEUSER } from '../../Services/userApi'
+import { OPENMODAL } from '../../Features/UserSlice';
+
 
 
 const UseDeleteUser = () => {
 
+
 const id = useSelector((state) => state?.user?.user?.userId)
-const dispatch = useDispatch()
+const isOpenModal = useSelector((state) => state?.user?.isDelete)
+const dispatch = useDispatch();
 
 const handleDeleteUser = async () => {
     await dispatch(DELETEUSER(id))
 }
 
-  return {handleDeleteUser}
+// define a function to open modal 
+const handleOpenModal = () => { 
+  dispatch(OPENMODAL());
+}
+
+  return {isOpenModal, handleDeleteUser, handleOpenModal}
 }
 
 export default UseDeleteUser
