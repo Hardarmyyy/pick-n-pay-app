c062dda231f1ec1b9f8654a6822b4dca134fb5c5
hardarmyyy <adeyemoadams005@gmail.com>
2024-07-20 11:37:15 +0100

    FIX: logo component

--- CLIENT/src/Layouts/Logo/Logo.jsx
+++ CLIENT/src/Layouts/Logo/Logo.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Logo = () => {
+const Logo = ({eventHandler}) => {
 
 
 return (
 
     <>
-        <nav className='text-blue-950 cursor-pointer font-Jost font-bold text-xl tablet:text-2xl mini:text-3xl laptop:text-4xl super:text-4xl'> 
+        <nav className='text-blue-950 cursor-pointer font-Jost font-bold text-xl tablet:text-2xl mini:text-3xl laptop:text-4xl super:text-4xl' onClick={eventHandler}> 
 
             <Link to='/'> <h1> Pick<span className='text-crimson'>N</span>Pay </h1> </Link> 
 
