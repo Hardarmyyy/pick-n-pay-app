ba6d7a2424345676d3c532d3e04d77f16e1198a1
hardarmyyy <adeyemoadams005@gmail.com>
2024-08-28 21:37:44 +0100

    FIX: update profile layout

--- CLIENT/src/Pages/UserProfile/ProfileLayout.jsx
+++ CLIENT/src/Pages/UserProfile/ProfileLayout.jsx
@@ -20,14 +20,24 @@ return (
 <>
     {profileStatus === 'Loading.......' && <Modal> <Spinner></Spinner> </Modal>}
 
-    <section className='w-full min-h-[70vh] py-6 sm:p-2 md:p-2 tablet:px-4 mini:px-6 laptop:px-6 super:px-60 flex justify-center items-start sm:items-center'>
+    <section className='w-full min-h-[70vh] py-6 sm:p-2 md:p-2 tablet:px-4 mini:px-6 laptop:px-6 super:px-60 flex justify-center items-start sm:items-center md:items-center'>
 
-        <div className='sm:hidden md:hidden tablet:w-1/5 mini:w-36 laptop:w-44 super:w-44 h-auto py-3 text-sm bg-gray-200 rounded-md shadow-sm text-my-primary font-Montserrat flex flex-col justify-center items-center'>
+        <div className='sm:hidden md:hidden tablet:w-1/5 mini:w-36 laptop:w-44 super:w-44 min-h-[100vh] py-3 text-sm bg-gray-200 rounded-md shadow-sm text-my-primary font-Montserrat'>
+            
+            <p className='mb-2 tablet:ms-3 mini:ms-3 laptop:ms-5  super:ms-5'>
+                <NavLink className={({ isActive }) => isActive ? "" : null} to={`/profile`}>  profile  </NavLink>
+            </p>
+            <p className='mb-2 tablet:ms-3 mini:ms-3 laptop:ms-5  super:ms-5'>
+                <NavLink className={({ isActive }) => isActive ? "" : null} to={`/profile/update-profile`}>  Update profile  </NavLink>
+            </p>
+            <p className='mb-2 tablet:ms-3 mini:ms-3 laptop:ms-5  super:ms-5'> 
+                <NavLink className={({ isActive }) => isActive ? "" : null} to={`/profile/update-password`}> Update password </NavLink> 
+            </p>
 
-            <p className='mb-2'><NavLink className={({ isActive }) => isActive ? "" : null} to={`/profile/update-profile`}>  Update profile  </NavLink></p>
-            <p className='mb-2'> <NavLink className={({ isActive }) => isActive ? "" : null} to={`/profile/update-password`}> Update password </NavLink> </p>
-            <Button margin= '5px 0px' eventHandler={handleSwicthProfile}> Switch profile </Button>
-            <Button padding='5px 5px' margin= '8px 0px' backgroundColor='crimson' eventHandler={handleOpenModal}> Delete account </Button>
+            <div className='tablet:ms-3 mini:ms-3 laptop:ms-5  super:ms-5'>
+                <Button margin= '5px 0px' eventHandler={handleSwicthProfile}> Switch profile </Button>
+                <Button padding='5px 5px' margin= '8px 0px' backgroundColor='crimson' eventHandler={handleOpenModal}> Delete account </Button>
+            </div>
 
         </div>
 
