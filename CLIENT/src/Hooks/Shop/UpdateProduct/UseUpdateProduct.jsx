140081c9f5a7a513ea70e74fe95a42eb3f2cdab2
hardarmyyy <adeyemoadams005@gmail.com>
2024-08-28 21:42:50 +0100

    FIX: update component to edit product and hooks to edit product

--- CLIENT/src/Hooks/Shop/UpdateProduct/UseUpdateProduct.jsx
+++ CLIENT/src/Hooks/Shop/UpdateProduct/UseUpdateProduct.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect, useState } from 'react';
 import {useNavigate} from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
-import { UPDATEPRODUCT } from '../../../Services/productAPi';
+import { UPDATEPRODUCT, SINGLEPRODUCT } from '../../../Services/productAPi';
 import UseValidateUpdateProduct from '../UpdateProduct/UseValidateUpdateProduct';
 
 
@@ -10,132 +10,151 @@ const UseUpdateProduct = (id) => {
 
 const dispatch = useDispatch()
 const navigate = useNavigate()
-const username = useSelector((state) => state.auth?.user?.userName)
-const storeProducts = useSelector((state) => state.product?.store)
-const currentProduct = storeProducts.find((product) => product?.productId === id)
 
-//define a state to handle the product information form;
+const currentProduct = useSelector((state) => state?.product?.selectedProduct);
+const categories = useSelector((state) => state.category?.allCategories)
+const productStatus = useSelector((state) => state.product?.status) 
+const username = useSelector((state) => state?.user?.user?.username)
+
+
 const [editProduct, setEditProduct] = useState({
-  title: currentProduct?.title,
-  price: currentProduct?.price,
-  description: currentProduct?.description,
-  category: currentProduct?.category,
-  brand: currentProduct?.brand,
-  countInStock: currentProduct?.countInStock
+    title: '',
+    price: '',
+    description: '',
+    category: '',
+    brand: '',
+    countInStock: ''
 })
 
-//define a state to handle error on input change validation
+// define a state to handle error on input change validation
 const [error, setError] = useState({})
-
+const [loading, setLoading] = useState(true)
 const [isSubmitting, setIsSubmitting] = useState(false)
 
-//define a function to handle product form change and validation
+// define a function to handle product form change and validation
 const handleChange = (e) => {
-  const {name, value} = e.target
+    const {name, value} = e.target
 
-  // Format price value to allow two decimal places
-  let formattedValue = value.replace(/[^\d.]/g, '');
-  const parts = formattedValue.split('.');
-  if (parts.length > 1) {
-      // If there are decimal parts, keep only two decimal places
-      formattedValue = `${parts[0]}.${parts[1].slice(0, 2)}`;
-  }
+    // Format price value to allow two decimal places
+    let formattedValue = value.replace(/[^\d.]/g, '');
+    const parts = formattedValue.split('.');
+    if (parts.length > 1) {
+        // If there are decimal parts, keep only two decimal places
+        formattedValue = `${parts[0]}.${parts[1].slice(0, 2)}`;
+    }
 
-  setEditProduct((editProduct) => {return {...editProduct, [name]: 
-      name === 'title' 
-          ? value 
-              : name === 'description' 
-                  ? value
-                    : name === 'category' 
-                        ? value
-                            : name === 'price' 
-                                ? formattedValue
-                                    : name === 'countInStock' 
-                                        ? value.replace(/[^0-9]/g, '')
-                                            : value.replace(/\s/g, "")
-  }})
+    setEditProduct((editProduct) => {return {...editProduct, [name]: 
+        name === 'price' 
+            ? formattedValue
+                : name === 'countInStock' 
+                    ? value.replace(/[^0-9]/g, '')
+                        : name === 'brand'
+                            ? value.replace(/\s/g, "")
+                                : value
+    }})
 
-  if (name === 'title') {
-      setError((error) => { return {...error, title: value ? value.length >= 100 ? 'Maximum number of characters is 100' : null : 'Kindly enter product title'}})
-  }
-  else if (name === 'price') {
-      setError((error) => { return {...error, price: value ? Number(value) <= 0 ? 'Minimum price must be greater than 0' : null : 'Kindly enter product price'}})
-  }
-  else if (name === 'description') {
-      setError((error) => { return {...error, description: value ? value.length >= 400 ? 'Maximum number of characters is 400' : null  : 'Kindly enter product description'}})
-  }
-  else if (name === 'category') {
-      setError((error) => { return {...error, category: value ? '' : 'Kindly select a category'}})
-  }
-  else if (name === 'brand') {
-      setError((error) => { return {...error, brand: value ? '' : null}})
-  }
-  else if (name === 'countInStock') {
-      setError((error) => { return {...error, countInStock: value ? Number(value) < 1 ? 'Minimum quantity is 1' : null : 'Kindly enter quantity in stock'}})
-  }
+    if (name === 'title') {
+        setError((error) => { return {...error, title: value ? value.length >= 100 ? 'Maximum number of characters is 100' : null : 'Kindly enter product title'}})
+    }
+    else if (name === 'price') {
+        setError((error) => { return {...error, price: value ? Number(value) <= 0 ? 'Minimum price must be greater than 0' : null : 'Kindly enter product price'}})
+    }
+    else if (name === 'description') {
+        setError((error) => { return {...error, description: value ? value.length >= 400 ? 'Maximum number of characters is 400' : null  : 'Kindly enter product description'}})
+    }
+    else if (name === 'category') {
+        setError((error) => { return {...error, category: value ? '' : 'Kindly select a category'}})
+    }
+    else if (name === 'brand') {
+        setError((error) => { return {...error, brand: value ? '' : null}})
+    }
+    else if (name === 'countInStock') {
+        setError((error) => { return {...error, countInStock: value ? Number(value) < 1 ? 'Minimum quantity is 1' : null : 'Kindly enter quantity in stock'}})
+    }
 }
 
-//import UseValidateUpdateProduct hooks to catch errors when submitting form
+// import UseValidateUpdateProduct hooks to catch errors when submitting form
 const {errors} = UseValidateUpdateProduct(editProduct)
 
 const handleCanUpload = (value) => {
-  const canUpload = [
-    value.title &&
-    value.price && 
-    Number(value.price) > 0 &&
-    value.description &&
-    value.category &&
-    value.countInStock &&
-    Number(value.countInStock) >= 1 
-  ].every(Boolean) // enable the submit button 
+    const canUpload = [
+        value.title &&
+        value.price && 
+        Number(value.price) > 0 &&
+        value.description &&
+        value.category &&
+        value.countInStock &&
+        Number(value.countInStock) >= 1 
+    ].every(Boolean) // enable the submit button 
 
-  return canUpload
+    return canUpload
 }
 
 const uploadProduct = handleCanUpload(editProduct)
 
-//define a function to upload new product
+// define a function to upload new product
 const handleUpdateProduct = async () => {
 
-  if (isSubmitting) return 
+    if (isSubmitting) return 
 
-  setError(errors)
+    setError(errors)
 
-  if (!isSubmitting && uploadProduct) {
-      setIsSubmitting(true); // Disable the button
-      await dispatch(UPDATEPRODUCT({id, username, editProduct}))
-      .then((response) => {
-            const {success, updatedProduct} = response.payload
+    if (!isSubmitting && uploadProduct) {
+        setIsSubmitting(true); // Disable the button
+        await dispatch(UPDATEPRODUCT({id, username, editProduct}))
+        .then((response) => {
+            const {success} = response.payload
             if (success) {
                 setEditProduct({
-                title: '',
-                price: '',
-                description: '',
-                category: '',
-                brand: '',
-                countInStock: ''
+                    title: '',
+                    price: '',
+                    description: '',
+                    category: '',
+                    brand: '',
+                    countInStock: ''
                 })
                 setTimeout(() => {
-                    navigate(`/shop/product-success/${updatedProduct._id}`)
+                    navigate(`/shop/all-products`)
                 }, 500)
         }
-      })
-      .catch((err) => {
-          toast.error('Something went wrong', {
-              toastStyle: { background: 'red', color: 'white' }
-          })
-      })
-      .finally(() => {
-          setIsSubmitting(false); // Re-enable the button on error
-      })
-  }
+        })
+        .catch((err) => {
+            toast.error('Something went wrong', {
+                toastStyle: { background: 'red', color: 'white' }
+            })
+        })
+        .finally(() => {
+            setIsSubmitting(false); // Re-enable the button on error
+        })
+    }
 }
 
 useEffect(() => {
-  handleCanUpload(editProduct)
+    handleCanUpload(editProduct)
 }, [editProduct])  
 
-return {currentProduct, editProduct, error, handleChange, handleUpdateProduct }
+
+// Use useEffect to set the editProduct state with current product information when the component mounts or current product changes
+useEffect(() => {
+    if (!currentProduct || currentProduct.productId !== id) {
+        dispatch(SINGLEPRODUCT({id, username}))
+    }
+    else {
+        setEditProduct({
+            title: currentProduct?.title || '',
+            price: currentProduct?.price || '',
+            description: currentProduct?.description || '',
+            category: currentProduct?.category || '',
+            brand: currentProduct?.brand || '',
+            countInStock: currentProduct?.countInStock || ''
+        });
+    }
+    return () => {
+        setLoading(false);
+    }
+}, [currentProduct, id, dispatch]);
+
+return {currentProduct, editProduct, error, handleChange, handleUpdateProduct, categories, productStatus, loading }
 
 
 }
--- CLIENT/src/Pages/Shop/UpdateProduct/UpdateProduct.jsx
+++ CLIENT/src/Pages/Shop/UpdateProduct/UpdateProduct.jsx
@@ -1,35 +1,48 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import UseUpdateProduct from '../../../Hooks/Shop/UpdateProduct/UseUpdateProduct';
-import ProductForm from '../NewProduct/ProductForm.jsx/ProductForm'
+import ProductForm from '../../../component/ProductForm.jsx/ProductForm' 
+import { Link } from 'react-router-dom';
+import UseFetchStore from '../../../Hooks/Shop/UseFetchStore';
+import Spinner from '../../../component/Spinner';
 
 const UpdateProduct = () => {
 
 const {id} = useParams()
-const {currentProduct, editProduct, error, handleChange, handleUpdateProduct} = UseUpdateProduct(id)
-
+const {shopStatus} = UseFetchStore()
+const {currentProduct, editProduct, error, handleChange, handleUpdateProduct, categories, productStatus, loading} = UseUpdateProduct(id) 
 
 const handleSubmitUpdateProduct = async (e) => {
     e.preventDefault()
     await handleUpdateProduct()
-}
+} 
 
   return (
     <>
-        <section className='w-3/4 px-10 py-6 font-Montserrat text-my-primary border rounded-md shadow-sm bg-white mx-auto translate-y-0'>
+        <section className='w-full tablet:w-3/4 mini:w-4/5 min-h-[70vh] laptop:w-4/5 super:w-4/5 mx-auto flex flex-col justify-center items-center'>
 
-            {currentProduct  
-                ?
+            {shopStatus === 'Loading...' && <Spinner></Spinner>} 
+
+            {currentProduct  && shopStatus !== 'Loading...' &&
+    
                     <ProductForm
                         newProduct={editProduct}
                         handleChange={handleChange}
                         error={error}
                         handleFormSubmit={handleSubmitUpdateProduct}
+                        categories={categories}
+                        status={productStatus}
                     >
                     </ProductForm>
-                    :
+            }
 
-                        <p className='text-center mt-4 text-lg'> The product does not exist </p>}
+            {!currentProduct  && shopStatus !== 'Loading...' && !loading &&
+
+                <div className='flex flex-col justify-center items-center'>
+                    <p className='font-Jost md:text-2xl lg:text-4xl mb-2'> The product does not exist </p>
+                    <p className='sm:text-sm md:text-sm tablet:text-lg mini:text-lg laptop:text-lg super:text-xl'> Click <Link to={'/shop/all-products'} className='text-blue-800 hover:underline font-bold'> here </Link> to see all products in the shop </p>
+                </div>
+            }
 
         </section>
     </>
